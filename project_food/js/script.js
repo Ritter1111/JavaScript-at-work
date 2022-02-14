d08b076aa766e11c28096324f650ca1ee32c9b1c
Angelina <angelina.liapin@gmail.com>
2022-02-14 16:43:08 +0200

    create tabs in a new project

--- project_food/js/script.js
+++ project_food/js/script.js
@@ -1,4 +1,39 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    const tabs = document.querySelectorAll()
+    const tabs = document.querySelectorAll('.tabheader__item'),
+    tabsContent = document.querySelectorAll('.tabcontent'),
+    tabsParent = document.querySelector('.tabheader__item');
+
+     function hideTabContent() {
+         tabsContent.forEach(item => {
+             item.style.display='none'
+         });
+
+         tabs.forEach(tab => {
+            item.classList.remove('tabheader__item_active');
+
+         });
+        }
+         function showTabContent(i=0) {
+             tabsContent[i].style.display='block';
+             tabs[i].classList.add('tabheader__item_active');
+         }
+
+
+         hideTabContent();
+         showTabContent();
+
+         tabsParent.addEventListener('click', (event) => {
+             const target = event.target;
+
+             if(target && target.classList.contains('tabheader__item')){
+                 tabs.forEach((item, i)=> {
+                     if(target == item) {
+                         hideTabContent();
+                         showTabContent(i);
+                     }
+
+                 });
+             }
+         });
 });
